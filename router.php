fdbc36547fb4564fe15ff5de27a22a5fb331bdf6
sai_local <sainath0030@gmail.com>
2024-07-03 22:48:22 +0100

    PHP route file created

---
+++ router.php
@@ -0,0 +1,24 @@
+<?php
+
+$url  = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes =   [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+];
+
+function abort($code =404){
+    http_response_code($code);
+    require "views/{$code}.php";
+    die();
+}
+function routeToController($url,$routes){
+    if(array_key_exists($url, $routes)){
+        require $routes[$url];
+    }else{
+      abort();
+    }
+}
+
+routeToController($url,$routes);

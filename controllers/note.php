6691b2178446d44229699a22126de68c207fdb87
sai_local <sainath0030@gmail.com>
2024-07-04 22:51:02 +0100

    Unautherized attempts check added Magic variables used for status checks

--- controllers/note.php
+++ controllers/note.php
@@ -4,6 +4,18 @@ $db = new Database($config['database']);
 
 $header =   "Note";
 
-$note   =   $db->query('SELECT * FROM notes where id = :id',['id' => $_GET['id']])->fetch();
+$note   =   $db->query('SELECT * FROM notes where id = :id', [
+    'id' => $_GET['id']
+])->fetch();
+
+if(! $note){
+    abort();
+}
+
+$currentUserId  =   9;
+
+if($note['user_id'] != $currentUserId){
+    abort(Response::FORBIDDEN);
+}
 
 include "views/note.view.php";
---
+++ views/403.php
@@ -0,0 +1,16 @@
+<?php include "partials/header.php"; ?>
+<?php include "partials/nav.php"; ?>
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <h1 class="text-2xl font-blod">
+        <p>You are not Autherized to view this Page</p>
+      </h1>
+      <p class="mt-4">
+          <a href="/">Back to Home</a>
+      </p>
+      
+    </div>
+  </main>
+</div>
+
+<?php include "partials/footer.view.php" ?>

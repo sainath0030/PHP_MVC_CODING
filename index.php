330801d7d86579830ebb2e06a268f76c6c55bf93
sai_local <sainath0030@gmail.com>
2024-07-04 14:20:54 +0100

    Database connection with class constructer

--- index.php
+++ index.php
@@ -4,16 +4,23 @@ require "functions.php";
 
 // connect to mysql database.
 
-$dsn    = "mysql:host=localhost;port=3306;dbname=myapp;user=root;password=Sai@1234;charset=utf8mb4";
+class Databse {
+    public $connection;
 
-$pdo = new PDO($dsn);
+    public function __construct(){
+        $dsn    = "mysql:host=localhost;port=3306;dbname=myapp;user=root;password=Sai@1234;charset=utf8mb4";
+        $this->connection =  new PDO($dsn);
+    }
+    
+    public function query($query){
+        $statement= $this->connection->prepare($query);
 
-$statement= $pdo->prepare("select * from posts");
+        $statement->execute();
 
-$statement->execute();
-
-$posts = $statement->fetchAll(pdo::FETCH_ASSOC);
-
-foreach($posts as $post){
-    echo "<li>". $post['title'] ."</li>";
+        return $statement;
+    }
 }
+
+$db = new Databse();
+$post   =   $db->query("select * from posts where id =1")->fetch(PDO::FETCH_ASSOC);
+DD($post['title']);

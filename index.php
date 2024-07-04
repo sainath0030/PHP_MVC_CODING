745db7f68ae51b2a1a24b186aa8495e6f4fffb9c
sai_local <sainath0030@gmail.com>
2024-07-04 20:52:21 +0100

    Databse file created Sql injection  super global  filter

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ require "Database.php";
 $config  = require "config.php";
 
 $db = new Database($config['database']);
-
-$posts   =   $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
+$id =   $_GET['id'];
+$query     = "select * from posts where id = ?";
+$posts   =   $db->query($query, [$id])->fetch();
 DD($posts);

2bbd94c362a4486c7690b9d2993cbe44f2c8b4ed
sai_local <sainath0030@gmail.com>
2024-07-04 14:09:37 +0100

    Mysql connection created with PDO Database table select query accesing

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 require "functions.php";
-require "router.php";
+//require "router.php";
+
+// connect to mysql database.
+
+$dsn    = "mysql:host=localhost;port=3306;dbname=myapp;user=root;password=Sai@1234;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement= $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(pdo::FETCH_ASSOC);
+
+foreach($posts as $post){
+    echo "<li>". $post['title'] ."</li>";
+}

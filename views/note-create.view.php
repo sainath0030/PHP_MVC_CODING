4c5b8c3fbcbc83119c7ed3a2bdad423e7862f303
sai_local <sainath0030@gmail.com>
2024-07-08 22:08:30 +0100

    Textarea style added

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -14,7 +14,7 @@
                   <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
                   <div class="mt-2">
                     <textarea id="body" name="body" rows="3" 
-                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        class="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         required>
                         <?= $_POST['body'] ?? '' ?>
                     </textarea>

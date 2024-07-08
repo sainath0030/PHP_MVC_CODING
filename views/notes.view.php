bbdfea982a0807119646725edbf1578b6f0dd9c4
sai_local <sainath0030@gmail.com>
2024-07-08 21:46:58 +0100

    Note create page added linked with database Notes inserted database with client and server validation

--- views/notes.view.php
+++ views/notes.view.php
@@ -5,14 +5,18 @@
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
       <h1>Hello wellcome to <?= $header ?> page</h1>
-      <p>
+      <ul>
         <?php foreach($notes as $note): ?>
           <li>
-            <a href="/note?id=<?= $note['id'] ?>"  class="" >
+            <a href="/note?id=<?= $note['id'] ?>"  class="text-blue-500 hover:underline" >
               <?= $note['body'] ?>
             </a>
         </li>
         <?php endforeach; ?>
+      </ul>
+
+      <p>
+        <a href="/notes/create" class="text-green-500 hover:underline"> Create Note</a>
       </p>
     </div>
   </main>
--- views/partials/header.php
+++ views/partials/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<script src="https://cdn.tailwindcss.com"></script>
+<script src="https://cdn.tailwindcss.com?plugins=forms"></script>
 <body class="h-full">
 
 <div class="min-h-full">

6f0ba29d019ceb4d393f969a7d75c376584a5b17
sai_local <sainath0030@gmail.com>
2024-07-11 21:41:20 +0100

    Route file upgraded to class file All method related routes functions created Router function created for performing routing Add methord created to reduce code reddundancy

--- Core/functions.php
+++ Core/functions.php
@@ -29,3 +29,9 @@ function view($path, $atributes = []){
 
     require base_path('views/'. $path );
 }
+
+function abort($code = 404){
+    http_response_code($code);
+    require base_path("views/{$code}.php");
+    die();
+}
--- routes.php
+++ routes.php
@@ -1,10 +1,28 @@
 <?php
 
-return   [
+/* return   [
     '/' => 'controllers/index.php',
     '/about' => 'controllers/about.php',
     '/contact' => 'controllers/contact.php',
     '/note'    => 'controllers/notes/show.php',
     '/notes'    => 'controllers/notes/index.php',
     '/notes/create' => 'controllers/notes/create.php',
-];
+]; */
+
+$router->get('/', 'controllers/index.php');
+
+$router->get('/about', 'controllers/about.php');
+
+$router->get('/contact', 'controllers/contact.php');
+
+$router->get('/note', 'controllers/notes/show.php');
+
+$router->delete('/note', 'controllers/notes/show.php');
+
+$router->get('/notes', 'controllers/notes/index.php');
+
+$router->get('/notes/create', 'controllers/notes/create.php');
+
+$router->post('/notes/create', 'controllers/notes/create.php');
+
+

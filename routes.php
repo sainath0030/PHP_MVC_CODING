8cb964168d4a1b5c51fcec3b9ad7335b4c4fb632
sai_local <sainath0030@gmail.com>
2024-07-12 19:31:07 +0100

    Service containers

--- routes.php
+++ routes.php
@@ -1,14 +1,5 @@
 <?php
 
-/* return   [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php',
-    '/note'    => 'controllers/notes/show.php',
-    '/notes'    => 'controllers/notes/index.php',
-    '/notes/create' => 'controllers/notes/create.php',
-]; */
-
 $router->get('/', 'controllers/index.php');
 
 $router->get('/about', 'controllers/about.php');
